1c8a97d5312248c0e30008755b8f23a9bf3ad13b
Zecreitas <schuh.gui@gmail.com>
2024-11-28 13:53:33 -0300

    first commit

--- index.js
+++ index.js
@@ -1,25 +1,29 @@
 require('dotenv').config();
 const express = require('express');
 const bodyParser = require('body-parser');
+const cors = require('cors');
 const connectToDatabase = require('./src/config/database');
 const userRoutes = require('./src/routes/routes');
 const curriculoRoutes = require('./src/routes/routesCurriculo');
 
 const app = express();
 
+app.use(cors({
+  origin: '*',
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  allowedHeaders: ['Content-Type', 'Authorization'],
+}));
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-// Conectar ao MongoDB
 connectToDatabase();
 
-// Configurar rotas
 app.use('/api', userRoutes);
 app.use('/uploads', express.static('uploads'));
 app.use('/api/curriculos', curriculoRoutes);
 
 const PORT = process.env.PORT || 3000;
-
 app.listen(PORT, () => {
   console.log(`Servidor rodando na porta ${PORT}`);
 });
